73968bb0416b07c3e2742d47dbe9cdda9df1d95c
Abdulla Bagishev <abdullahbagishev@icloud.com>
2023-12-21 20:53:20 +0100

    create new feature: check for identical code with AST

---
+++ check_with_ast.py
@@ -0,0 +1,15 @@
+import ast
+
+
+def get_ast_string(code):
+    try:
+        tree = ast.parse(code)
+        return ast.dump(tree)
+    except SyntaxError:
+        return None
+
+
+def are_asts_identical(code1, code2):
+    ast1 = get_ast_string(code1)
+    ast2 = get_ast_string(code2)
+    return ast1 is not None and ast2 is not None and ast1 == ast2
