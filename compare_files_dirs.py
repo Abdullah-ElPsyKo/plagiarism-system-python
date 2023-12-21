fd1b8459b16153a75867f3d6007d065e48054214
Abdulla Bagishev <abdullahbagishev@icloud.com>
2023-12-21 20:54:39 +0100

    rename check_comment function to check_content and implement code check with ast

--- compare_files_dirs.py
+++ compare_files_dirs.py
@@ -2,6 +2,7 @@ import filecmp
 from pathlib import Path
 import re
 from cst_comments import *
+from check_with_ast import *
 
 def compare_files_in_directories(dir_check): # dir_check is a path to a directory
     dir_check = Path(dir_check)
@@ -15,7 +16,7 @@ def compare_files_in_directories(dir_check): # dir_check is a path to a director
             plagiarized_files = compare_two_directories(dir1, dir2)
             if plagiarized_files:
                 results.append((dir1.name, dir2.name, plagiarized_files))
-            comment_results = check_comments(dir1, dir2, plagiarized_files)
+            comment_results = check_content(dir1, dir2, plagiarized_files)
             plagiarized_comments.append(comment_results)
 
     return (results, plagiarized_comments)
@@ -33,13 +34,14 @@ def compare_two_directories(dir1, dir2):
     return matches
 
 
-def check_comments(dir1, dir2, identical_files=None):
+def check_content(dir1, dir2, identical_files=None):
     dir1 = Path(dir1)
     dir2 = Path(dir2)
     files_dir1 = list(dir1.glob('**/*.py'))
     files_dir2 = list(dir2.glob('**/*.py'))
 
     identical_comments = set()
+    ast_check = []
 
     for file1 in files_dir1:
         with open(file1, 'r') as f1:
@@ -50,10 +52,14 @@ def check_comments(dir1, dir2, identical_files=None):
                     continue
                 with open(file2, 'r') as f2:
                     file2_content = f2.read()
+                    ast_check.append((file1.name, file2.name, are_asts_identical(file1_content, file2_content)))
                     identical_cst = check_cst_comments(file1_content, file2_content)
                     file2_comments = set(re.findall(r'#(.*)', file2_content))
                     identical_comments.update(file1_comments.intersection(file2_comments))
-    return (dir1.name, dir2.name, identical_comments, identical_cst)
+    return (dir1.name, dir2.name, identical_comments, identical_cst, ast_check)
+
+
+
 
 
 if __name__ == "__main__":
